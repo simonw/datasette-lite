1da421a93c296ba419cdd30e26e727ed85a64b2f
Simon Willison <swillison@gmail.com>
2022-05-03 14:03:29 -0700

    Support loading database by URL, closes #6

--- webworker.js
+++ webworker.js
@@ -5,38 +5,53 @@ function log(line) {
   self.postMessage({type: 'log', line: line});
 }
 
-async function startDatasette() {
+async function startDatasette(initialUrl) {
+  let toLoad = [];
+  if (initialUrl) {
+    let name = initialUrl.split('.db')[0].split('/').slice(-1)[0];
+    toLoad.push([name, initialUrl]);
+  } else {
+    toLoad.push(["fixtures.db", "https://latest.datasette.io/fixtures.db"]);
+    toLoad.push(["content.db", "https://datasette.io/content.db"]);
+  }
+  console.log({toLoad});
   self.pyodide = await loadPyodide({
     indexURL: "https://cdn.jsdelivr.net/pyodide/dev/full/"
   });
   await pyodide.loadPackage('micropip', log);
   await pyodide.loadPackage('ssl', log);
   await pyodide.loadPackage('setuptools', log); // For pkg_resources
-  await self.pyodide.runPythonAsync(`
-  # Grab that fixtures.db database
-  from pyodide.http import pyfetch
-  names = []
-  for name, url in (
-      ("fixtures.db", "https://latest.datasette.io/fixtures.db"),
-      ("content.db", "https://datasette.io/content.db"),
-  ):
-      response = await pyfetch(url)
-      with open(name, "wb") as fp:
-          fp.write(await response.bytes())
-      names.append(name)
+  try {
+    await self.pyodide.runPythonAsync(`
+    # Grab that fixtures.db database
+    from pyodide.http import pyfetch
+    names = []
+    for name, url in ${JSON.stringify(toLoad)}:
+        response = await pyfetch(url)
+        with open(name, "wb") as fp:
+            fp.write(await response.bytes())
+        names.append(name)
 
-  import micropip
-  # Workaround for Requested 'h11<0.13,>=0.11', but h11==0.13.0 is already installed
-  await micropip.install("h11==0.12.0")
-  await micropip.install("datasette==0.62a0")
-  from datasette.app import Datasette
-  ds = Datasette(names, memory=True, settings={"num_sql_threads": 0})
-  `);
+    import micropip
+    # Workaround for Requested 'h11<0.13,>=0.11', but h11==0.13.0 is already installed
+    await micropip.install("h11==0.12.0")
+    await micropip.install("datasette==0.62a0")
+    from datasette.app import Datasette
+    ds = Datasette(names, settings={"num_sql_threads": 0})
+    `);
+  } catch (error) {
+    self.postMessage({error: error.message});
+  }
 }
 
-let readyPromise = startDatasette();
+let readyPromise = null;
 
 self.onmessage = async (event) => {
+  console.log({event, data: event.data});
+  if (event.data.type == 'startup') {
+    readyPromise = startDatasette(event.data.initialUrl);
+    return;
+  }
   // make sure loading is done
   await readyPromise;
   console.log(event, event.data);
@@ -53,6 +68,6 @@ self.onmessage = async (event) => {
     );
     self.postMessage({status, contentType, text});
   } catch (error) {
-    self.postMessage({ error: error.message });
+    self.postMessage({error: error.message});
   }
 };
