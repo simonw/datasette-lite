caa8eade10f0321c64f9f65c4561186f02d57c5b
Simon Willison <swillison@gmail.com>
2022-06-20 12:06:19 -0700

    Load a single CSV file from a URL, refs #32

--- webworker.js
+++ webworker.js
@@ -5,14 +5,19 @@ function log(line) {
   self.postMessage({type: 'log', line: line});
 }
 
-async function startDatasette(initialUrl) {
+async function startDatasette(settings) {
   let toLoad = [];
-  if (initialUrl) {
+  let csvs = [];
+  if (settings.initialUrl) {
     let name = initialUrl.split('.db')[0].split('/').slice(-1)[0];
     toLoad.push([name, initialUrl]);
-  } else {
+  } else if (!settings.csvUrl) {
     toLoad.push(["fixtures.db", "https://latest.datasette.io/fixtures.db"]);
     toLoad.push(["content.db", "https://datasette.io/content.db"]);
+  }
+  if (settings.csvUrl) {
+    csvs.push(settings.csvUrl);
+    toLoad.push(["data.db", 0]);
   }
   console.log({toLoad});
   self.pyodide = await loadPyodide({
@@ -24,18 +29,39 @@ async function startDatasette(initialUrl) {
   try {
     await self.pyodide.runPythonAsync(`
     # Grab that fixtures.db database
+    import sqlite3
     from pyodide.http import pyfetch
     names = []
     for name, url in ${JSON.stringify(toLoad)}:
-        response = await pyfetch(url)
-        with open(name, "wb") as fp:
-            fp.write(await response.bytes())
+        if url:
+            response = await pyfetch(url)
+            with open(name, "wb") as fp:
+                fp.write(await response.bytes())
+        else:
+            sqlite3.connect(name).execute("vacuum")
         names.append(name)
 
     import micropip
     # Workaround for Requested 'h11<0.13,>=0.11', but h11==0.13.0 is already installed
     await micropip.install("h11==0.12.0")
     await micropip.install("datasette==0.62a0")
+    csvs = ${JSON.stringify(csvs)}
+    if csvs:
+        await micropip.install("sqlite-utils==3.27")
+        import sqlite_utils
+        from sqlite_utils.utils import rows_from_file, TypeTracker
+        db = sqlite_utils.Database("data.db")
+        for csv_url in csvs:
+            tracker = TypeTracker()
+            response = await pyfetch(csv_url)
+            with open("csv.csv", "wb") as fp:
+                fp.write(await response.bytes())
+            db["csv"].insert_all(
+                tracker.wrap(rows_from_file(open("csv.csv", "rb"))[0])
+            )
+            db["csv"].transform(
+                types=tracker.types
+            )
     from datasette.app import Datasette
     ds = Datasette(names, settings={
         "num_sql_threads": 0,
@@ -60,7 +86,7 @@ let readyPromise = new Promise(function(resolve) {
 self.onmessage = async (event) => {
   console.log({event, data: event.data});
   if (event.data.type == 'startup') {
-    await startDatasette(event.data.initialUrl);
+    await startDatasette(event.data);
     return;
   }
   // make sure loading is done
