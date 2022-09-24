cc89e348784dd730de62a0a1ae7b154830262701
Nelson Chen <crazysim@gmail.com>
2022-09-24 01:45:22 -0700

    20 minutes. Might as well just download the DB!

--- webworker.js
+++ webworker.js
@@ -282,7 +282,7 @@ async function startDatasette(settings) {
     from datasette.app import Datasette
     ds = Datasette(names, settings={
         "num_sql_threads": 0,
-        "sql_time_limit_ms": 600000,
+        "sql_time_limit_ms": 1200000,
     }, metadata = {
         "about": "Datasette Lite",
         "about_url": "https://github.com/simonw/datasette-lite"
